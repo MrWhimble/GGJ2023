87b04a4ccee5bb4f7cdb1f9634ea08c0b00ce5ad
Lewis Bonner <lewis007bonner@hotmail.co.uk>
2023-02-03 23:21:57 +0000

    gyrotest

--- Assets/_Project/Scripts/PlayerController.cs
+++ Assets/_Project/Scripts/PlayerController.cs
@@ -6,11 +6,25 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float speed;
 
+    private Camera _camera;
+
+    private Vector2 _topLeft;
+    private Vector2 _bottomRight;
+
+    private void Awake()
+    {
+        _camera = Camera.main;
+        _topLeft = _camera.ScreenToWorldPoint(new Vector3(0, 0, 0));
+        _bottomRight = _camera.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height));
+    }
+    
     private void Update()
     {
         Vector2 inputDir = PlayerInput.GetMoveDirection(transform.position);
         
         transform.Translate(inputDir * speed * Time.deltaTime);
+
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, _topLeft.x, _bottomRight.x), Mathf.Clamp(transform.position.y, _topLeft.y, _bottomRight.y), 0);
     }
     
 }
--- Assets/_Project/Scripts/PlayerInput.cs
+++ Assets/_Project/Scripts/PlayerInput.cs
@@ -18,9 +18,13 @@ public class PlayerInput : MonoBehaviour
     private Gyroscope _gyro;
 
     private Camera _camera;
-    
-    private bool noTouchesLastFrame;
-    private int touchId;
+
+
+    private Quaternion _originalRotation;
+    private Vector3 _originalForward;
+    private float _originalAngle;
+    private bool _noTouchesLastFrame;
+    private int _touchId;
     private bool _following;
     private Vector2 _followPos;
     
@@ -30,13 +34,19 @@ public class PlayerInput : MonoBehaviour
 
         _camera = Camera.main;
         
-        if (!Input.gyro.enabled && inputType is InputTypes.Gyro)
+        if (!SystemInfo.supportsGyroscope && inputType is InputTypes.Gyro)
         {
             Debug.LogError("Device doesn't have a gyro!!!");
             inputType = InputTypes.Follow;
         }
         _gyro = Input.gyro;
         _gyro.enabled = true;
+        _originalRotation = _gyro.attitude;
+        _originalForward = _originalRotation * Vector3.up;
+        _originalForward.y = 0;
+        _originalAngle = Vector3.SignedAngle(_originalForward, Vector3.up, Vector3.forward);
+        Debug.Log(_originalAngle);
+        _originalAngle *= Mathf.Deg2Rad;
     }
 
     public static Vector2 GetMoveDirection(Vector2 currentPosition)
@@ -56,7 +66,17 @@ public class PlayerInput : MonoBehaviour
             }
             case InputTypes.Gyro:
             {
-                return new Vector2(0, 0);
+                //Vector3 currentUp = _instance._gyro.attitude * Vector3.up;
+                //Vector3 input = Vector3.ProjectOnPlane(currentUp, _instance._originalUp);
+                //input = Quaternion.Inverse(_instance._originalRotation) * input;
+                //float forward = Vector3.Project(input, Vector3.forward);
+                //input = Quaternion.Inverse(_instance._originalRotation) * input;
+                Vector3 newForward = _instance._gyro.attitude * (Quaternion.Inverse(_instance._originalRotation) * Vector3.forward);
+                newForward = Quaternion.Inverse(_instance._originalRotation) * newForward;
+                float sin = Mathf.Sin(_instance._originalAngle);
+                float cos = Mathf.Cos(_instance._originalAngle);
+                
+                return new Vector2(cos * newForward.x - sin * newForward.y, sin * newForward.x + cos * newForward.y);
             }
         }
         return Vector2.zero;
@@ -76,20 +96,20 @@ public class PlayerInput : MonoBehaviour
             else
             {
                 Touch touch = default;
-                if (noTouchesLastFrame && Input.touchCount != 0)
+                if (_noTouchesLastFrame && Input.touchCount != 0)
                 {
                     touch = Input.GetTouch(0);
-                    touchId = touch.fingerId;
+                    _touchId = touch.fingerId;
                     if (touch.phase == TouchPhase.Ended)
                     {
-                        touchId = -1;
+                        _touchId = -1;
                         touch = default;
                     }
-                } else if (Input.touchCount > 0 && !noTouchesLastFrame)
+                } else if (Input.touchCount > 0 && !_noTouchesLastFrame)
                 {
                     foreach (Touch t in Input.touches)
                     {
-                        if (t.fingerId != touchId)
+                        if (t.fingerId != _touchId)
                             continue;
                         touch = t;
                     }
@@ -101,7 +121,7 @@ public class PlayerInput : MonoBehaviour
                     _following = true;
                 }
                 
-                noTouchesLastFrame = Input.touchCount == 0;
+                _noTouchesLastFrame = Input.touchCount == 0;
             }
 
             if (!_following)
